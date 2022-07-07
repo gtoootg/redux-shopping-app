222fddd43fe08b6ec071126cac0dbe4d9cfe6ead
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-07-07 15:25:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
+
+function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  console.log(isLoggedIn);
+  return <div className="App">{!isLoggedIn ? <Auth /> : <Layout />}</div>;
+}
+
+export default App;
